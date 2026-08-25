f3a423f95a9e2d66be184f33a533d7a83c33e83f
sekigan98 <sekigan98@gmail.com>
2026-08-25 15:31:16 -0300

    Delete src directory

--- src/routes/public.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getPricingForRequest } from '../lib/pricing.js';
-
-export const publicRouter = express.Router();
-
-publicRouter.get('/pricing', (req, res) => {
-  res.json(getPricingForRequest(req));
-});
